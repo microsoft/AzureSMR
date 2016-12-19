3a18462a38b7adf88485d54d17c26b99791c4b6c
Andrie de Vries <apdevries@gmail.com>
2016-12-19 17:36:53 +0000

    Create helper functions #10

--- R/internal.R
+++ R/internal.R
@@ -3,3 +3,18 @@ stopWithAzureError <- function(r){
   message(content(r)$error$message)
   stop("Error return code: ", status_code(r), call. = FALSE)
 }
+
+extractResourceGroupName <- function(x) gsub(".*?/resourceGroups/(.*?)(/.*)*$", "\\1", x)
+extractSubscriptionID    <- function(x) gsub(".*?/subscriptions/(.*?)(/.*)*$",  "\\1", x)
+
+refreshStorageKey <- function(AzureActiveContext){
+  if (length(AzureActiveContext$StorageAccountK) < 1 ||
+      SAI != AzureActiveContext$StorageAccountK ||
+      length(AzureActiveContext$StorageKey) <1
+  ) {
+    message("Fetching Storage Key..")
+    AzureSAGetKey(AzureActiveContext, ResourceGroup = RGI, StorageAccount = SAI)
+  } else {
+    AzureActiveContext$StorageKey
+  }
+}
