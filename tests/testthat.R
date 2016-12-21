23f3d5652fa6753fe3e523d721a17e9ec8e9844a
Andrie de Vries <apdevries@gmail.com>
2016-12-21 12:26:16 +0000

    Rename AzureSM to AzureSMR #14

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,12 +3,12 @@ library(testthat, quietly = TRUE)
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   # NOT_CRAN
   # run all tests
-  test_check("AzureSM")
-  
+  test_check("AzureSMR")
+
 } else {
   # CRAN
   # skip some tests on CRAN, to comply with timing directive and other policy
-  test_check("AzureSM")
-  # test_check("AzureSM", filter = "1-workspace-no-config")
-  # test_check("AzureSM", filter = "7-discover-schema")
+  test_check("AzureSMR")
+  # test_check("AzureSMR", filter = "1-workspace-no-config")
+  # test_check("AzureSMR", filter = "7-discover-schema")
 }
