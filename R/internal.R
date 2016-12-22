8452ed28ed9735698d5b54aac17ad13c297d2186
Andrie de Vries <apdevries@gmail.com>
2016-12-22 06:58:14 +0000

    Display error message for xml_document objects #34

--- R/internal.R
+++ R/internal.R
@@ -1,13 +1,18 @@
 stopWithAzureError <- function(r){
-  if(!is.null(r$error$code))
-    message(content(r)$error$code)
-  if(!is.null(r$error$message))
-    message(content(r)$error$message)
+  if(inherits(content(r), "xml_document")){
+    rr <- XML::xmlToList(XML::xmlParse(content(r)))
+    if(!is.null(rr$Code)) message(rr$Code)
+    if(!is.null(rr$Message)) message(rr$Message)
+  } else {
+    if(!is.null(r$error$code)) message(content(r)$error$code)
+    if(!is.null(r$error$message)) message(content(r)$error$message)
+  }
   stop("Error return code: ", status_code(r), call. = FALSE)
 }
 
-extractResourceGroupname <- function(x) gsub(".*?/resourceGroups/(.*?)(/.*)*$", "\\1", x)
-extractSubscriptionID    <- function(x) gsub(".*?/subscriptions/(.*?)(/.*)*$",  "\\1", x)
+extractResourceGroupname <- function(x) gsub(".*?/resourceGroups/(.*?)(/.*)*$",  "\\1", x)
+extractSubscriptionID    <- function(x) gsub(".*?/subscriptions/(.*?)(/.*)*$",   "\\1", x)
+extractStorageAccount    <- function(x) gsub(".*?/storageAccounts/(.*?)(/.*)*$", "\\1", x)
 
 refreshStorageKey <- function(azureActiveContext, storageAccount, resourceGroup){
   if (length(azureActiveContext$storageAccountK) < 1 ||
