2489cbe93a1e12d1e83cf8b9219ab814792fe490
Andrie de Vries <apdevries@gmail.com>
2017-06-06 16:58:16 -0400

    Create RStudio addins #72

---
+++ R/addins.R
@@ -0,0 +1,45 @@
+# this is a template to view a DT object
+# @param data.frame
+# @param title Text to display in pane title
+
+#' @importFrom miniUI miniPage gadgetTitleBar miniContentPanel
+#' @importFrom DT dataTableOutput renderDataTable
+#' @importFrom shiny observeEvent paneViewer runGadget
+dataTableViewer <- function(x, title = "") {
+
+  ui <- miniPage(
+    gadgetTitleBar(title),
+    miniContentPanel(
+      DT::dataTableOutput("dt", height = "100%")
+    )
+  )
+
+  server <- function(input, output, session) {
+    output$dt <- DT::renderDataTable(x)
+
+    observeEvent(input$done, stopApp())
+  }
+
+  viewer <- paneViewer()
+  runGadget(ui, server, viewer = viewer)
+
+}
+
+
+addinListAllResources <- function() {
+  settingsfile <- getOption("AzureSMR.config")
+  assert_that(file.exists(settingsfile))
+  az <- createAzureContext(configFile = settingsfile)
+  z <- azureListAllResources(az)
+  z$id <- NULL
+  dataTableViewer(z, title = "All resources")
+}
+
+addinGetAllVMstatus <- function() {
+  settingsfile <- getOption("AzureSMR.config")
+  assert_that(file.exists(settingsfile))
+  az <- createAzureContext(configFile = settingsfile)
+  z <- azureGetAllVMstatus(az)
+  z$id <- NULL
+  dataTableViewer(z, title = "All virtual machines")
+}
