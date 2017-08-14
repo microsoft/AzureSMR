c1e2665f13e25dc8a4bab8a878fa6e95f65051ee
omkarksa <omkarksa@yahoo.co.uk>
2017-08-14 13:32:48 +0530

    ADLS related cleanup

--- R/AzureDataLake.R
+++ R/AzureDataLake.R
@@ -1,42 +1,57 @@
 
-#' Azure Data Lake LISTSTATUS for specified storage account.
+#' Azure Data Lake LISTSTATUS for specified relativePath of an azure data lake account.
 #'
 #' @inheritParams createAzureContext
 #' @inheritParams azureAuthenticate
 #'
 #' @param azureActiveContext Provide an `azureActiveContext` object used for authentication.
-#' @param azureDataLakeStoreAccount Provide the name of the Azure Data Lake Store account.
+#' @param azureDataLakeAccount Provide the name of the Azure Data Lake account.
+#' @param relativePath Provide a relative path of the directory.
+#' @param verbose Print tracing information (default FALSE).
 #'
-#' @return Returns a data frame. This data frame has an attribute called `marker` that can be used with the `marker` argument to return the next set of values.
+#' @return Returns a data frame.
 #'
 #' @template
 #' @references
 #' @family Azure Data Lake Store functions
 #' @export
-azureDataLakeListStatus <- function(azureActiveContext, storageAccount, relativePath = "", verbose = FALSE) {
+azureDataLakeListStatus <- function(azureActiveContext, azureDataLakeAccount, relativePath = "", verbose = FALSE) {
 
   if (!missing(azureActiveContext) && !is.null(azureActiveContext)) {
     assert_that(is.azureActiveContext(azureActiveContext))
     azureCheckToken(azureActiveContext)
-    if(missing(storageAccount)) storageAccount <- azureActiveContext$storageAccount
   }
 
-  #assert_that(is_storage_account(storageAccount))
+  # ToDo: Need to check if ADLS requires a different one
+  assert_that(is_storage_account(azureDataLakeAccount))
 
   verbosity <- set_verbosity(verbose)
 
-  URL <- paste0("https://", storageAccount, ".azuredatalakestore.net/webhdfs/v1/", relativePath, "?op=LISTSTATUS&api-version=2016-11-01")
+  URL <- paste0(
+    "https://", azureDataLakeAccount, ".azuredatalakestore.net/webhdfs/v1/",
+    relativePath,
+    "?op=LISTSTATUS",
+    "&api-version=2016-11-01"
+    )
 
-  r <- callAzureDataLakeApi(URL,
+  resHttp <- callAzureDataLakeApi(URL,
     azureActiveContext = azureActiveContext,
     verbose = verbose)
 
-  if (status_code(r) == 404) {
+  if (status_code(resHttp) == 404) {
     warning("Azure data lake response: resource not found")
     return(NULL)
   }
-  stopWithAzureError(r)
+  stopWithAzureError(resHttp)
 
-  jsonlite::toJSON(jsonlite::fromJSON(content(r, "text", encoding = "UTF-8")), pretty = TRUE)
+  resJsonStr <- content(resHttp, "text", encoding = "UTF-8")
+  if (length(resJsonStr) == 0) {
+    return(
+      as.data.frame()
+    )
+  }
+  resJson <- jsonlite::fromJSON(resJsonStr)
+  resDf <- as.data.frame(resJson)
+  resDf
 }
 
