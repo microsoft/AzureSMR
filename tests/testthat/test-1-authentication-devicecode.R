8d80434f9b95e9bf6cc0fbe62b2f140c38228893
omkarksa <omkarksa@yahoo.co.uk>
2017-09-29 19:32:51 +0530

    ADLS- DeviceCode flow - Added a manual test with instructions for DeviceCode flow.

---
+++ tests/testthat/test-1-authentication-devicecode.R
@@ -0,0 +1,45 @@
+#' -----------------------------------------------------------------------------
+#' DESCRIPTION:
+#' This is a manual test for testing device code flow of authentication, since 
+#' this waits till the user can manually authenticate the provided DeviceCode. 
+#' 
+#' TO RUN:
+#' ** Set correct path of "config_devicecode.json" in the below "settingsfile".
+#' ** setwd("<root>/AzureSMR/tests/")
+#' ** test_check("AzureSMR", filter = "1-authentication-devicecode")
+#' 
+#' SAMPLE config_devicecode.json:
+#' 
+#' {
+#' "authType": "DeviceCode",
+#' "resource": "https://datalake.azure.net/",
+#' "tenantID": "72fblahf-blah-41af-blah-2d7cBLAHdb47",
+#' "clientID": "2d302a05-86f0-4e0a-a8f6-ae4d28a035df",
+#' }
+#' -----------------------------------------------------------------------------
+
+
+if(interactive()) library("testthat")
+
+settingsfile <- "E:/Projects/R/config_devicecode.json" #system.file("tests/testthat/config_devicecode.json", package = "AzureSMR")
+
+#  ------------------------------------------------------------------------
+
+context("Authenticate")
+
+test_that("Can connect to workspace with config file using device code", {
+  skip_if_missing_config(settingsfile)
+  
+  config <- read.AzureSMR.config(settingsfile)
+  asc <- createAzureContext()
+  with(config,
+       setAzureContext(asc, tenantID = tenantID, clientID = clientID, authType = authType, resource = resource)
+  )
+  expect_is(asc, "azureActiveContext")
+  
+  res <- azureAuthenticateOnAuthType(asc)
+  expect_true(res)
+  
+  expect_gt(as.numeric(asc$EXPIRY), as.numeric(Sys.time() + 3590))
+})
+
